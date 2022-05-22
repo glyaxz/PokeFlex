46f21f05f4be9b2d632dce43e6f82aca682d9104
Glyaxz <javiky03@gmail.com>
2022-05-22 22:33:09 +0200

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -2,16 +2,15 @@ const header = document.querySelector('header');
 const section = document.querySelector('section');
 let Gen=1
 
-var data = new FormData();
-data.append('json_url','http://127.0.0.1/api/pokedex.api');
+// var data = new FormData();
+// data.append('json_url','http://127.0.0.1/api/pokedex.api');
 
-let req = XMLHttpRequest();
-req.open('POST', '127.0.0.1', false);
+const req = XMLHttpRequest();
+req.open('GET', 'localhost/api/pokedex.json', false);
 req.responsiveType = 'json';
-req.send(data);
+req.send();
 
 req.onload = function(){
-	req.responsiveType="json"
 	jsonResp = $.parse(req.response);
 	for(let i = 0; i<jsonResp[Gen-1].length; i++){
 		console.log(111)
