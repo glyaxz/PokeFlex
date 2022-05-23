bf94ed700d2c28b03bfd725ca594fd368d64b3a9
Glyaxz <javiky03@gmail.com>
2022-05-23 19:39:30 +0200

    footer added

--- index.php
+++ index.php
@@ -44,11 +44,81 @@
 			</div>
 		</div>
 
-		<div id="pokeCards" class="pokecard">
+		<div id="pokeCards" class="pokecards">
 			<!-- Aqui se introduciran via JS las cartas dependiendo la gen que se escoja -->
 		</div>
 
 	</section>
+
+	<footer class="bg-light text-center text-white">
+		<!-- Grid container -->
+		<div class="container p-4 pb-0">
+			<!-- Section: Social media -->
+			<section class="mb-4">
+			<!-- Facebook -->
+			<a
+				class="btn btn-primary btn-floating m-1"
+				style="background-color: #3b5998;"
+				href="#!"
+				role="button"
+				><i class="fab fa-facebook-f"></i
+			></a>
+
+			<!-- Twitter -->
+			<a
+				class="btn btn-primary btn-floating m-1"
+				style="background-color: #55acee;"
+				href="#!"
+				role="button"
+				><i class="fab fa-twitter"></i
+			></a>
+
+			<!-- Google -->
+			<a
+				class="btn btn-primary btn-floating m-1"
+				style="background-color: #dd4b39;"
+				href="#!"
+				role="button"
+				><i class="fab fa-google"></i
+			></a>
+
+			<!-- Instagram -->
+			<a
+				class="btn btn-primary btn-floating m-1"
+				style="background-color: #ac2bac;"
+				href="#!"
+				role="button"
+				><i class="fab fa-instagram"></i
+			></a>
+
+			<!-- Linkedin -->
+			<a
+				class="btn btn-primary btn-floating m-1"
+				style="background-color: #0082ca;"
+				href="#!"
+				role="button"
+				><i class="fab fa-linkedin-in"></i
+			></a>
+			<!-- Github -->
+			<a
+				class="btn btn-primary btn-floating m-1"
+				style="background-color: #333333;"
+				href="#!"
+				role="button"
+				><i class="fab fa-github"></i
+			></a>
+			</section>
+			<!-- Section: Social media -->
+		</div>
+
+
+		<!-- Copyright -->
+		<div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+			© 2020 Copyright:
+			<a class="text-white" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+		</div>
+		<!-- Copyright -->
+		</footer>
 	<script src="js/functions.js"></script>
 	<script src="js/jq.js"></script>
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
