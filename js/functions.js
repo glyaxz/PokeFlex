b3deeadc2f1898aa22357d5a1b56b9a8f425c831
Glyaxz <javiky03@gmail.com>
2022-05-23 20:22:44 +0200

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -47,23 +47,7 @@ function loadGen(generation,json){
 
 		document.getElementById('pokeCards').appendChild(myDiv)
 
-		popup.appendChild(myDiv)
-		
-		myDiv.addEventListener('click', function(){
-			myDiv.addEventListener('click', () => {
-				popup.style.display = 'block';
-			})
-			close.addEventListener('click', () => {
-				popup.style.display = 'none';
-			});
-			 
-			popup.addEventListener('click', e => {
-				if(e.target.className === 'popup-wrapper') {
-					popup.style.display = 'none';
-				}
-			});
-		}
-		 )
+
 	}
 
 }
