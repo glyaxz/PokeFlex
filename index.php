f7de0a75f44e7c46faf6e9fa944695c48ee29ea1
Glyaxz <javiky03@gmail.com>
2022-05-23 19:56:15 +0200

    1

--- index.php
+++ index.php
@@ -14,7 +14,6 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Document</title>
 	<link rel="stylesheet" href="css/styles.css">
-	<link rel="stylesheet" href="css/bootstrap.css">
 	<link rel="icon" href="favicon.png" type="image/png">
 </head>
 
@@ -49,76 +48,77 @@
 		</div>
 
 	</section>
-
-	<footer class="bg-light text-center text-white">
+	<br>
+	<footer class="text-center text-white" style="background-color: #f1f1f1;">
 		<!-- Grid container -->
-		<div class="container p-4 pb-0">
+		<div class="container pt-4">
 			<!-- Section: Social media -->
 			<section class="mb-4">
 			<!-- Facebook -->
 			<a
-				class="btn btn-primary btn-floating m-1"
-				style="background-color: #3b5998;"
+				class="btn btn-link btn-floating btn-lg text-dark m-1"
 				href="#!"
 				role="button"
+				data-mdb-ripple-color="dark"
 				><i class="fab fa-facebook-f"></i
 			></a>
 
 			<!-- Twitter -->
 			<a
-				class="btn btn-primary btn-floating m-1"
-				style="background-color: #55acee;"
+				class="btn btn-link btn-floating btn-lg text-dark m-1"
 				href="#!"
 				role="button"
+				data-mdb-ripple-color="dark"
 				><i class="fab fa-twitter"></i
 			></a>
 
 			<!-- Google -->
 			<a
-				class="btn btn-primary btn-floating m-1"
-				style="background-color: #dd4b39;"
+				class="btn btn-link btn-floating btn-lg text-dark m-1"
 				href="#!"
 				role="button"
+				data-mdb-ripple-color="dark"
 				><i class="fab fa-google"></i
 			></a>
 
 			<!-- Instagram -->
 			<a
-				class="btn btn-primary btn-floating m-1"
-				style="background-color: #ac2bac;"
+				class="btn btn-link btn-floating btn-lg text-dark m-1"
 				href="#!"
 				role="button"
+				data-mdb-ripple-color="dark"
 				><i class="fab fa-instagram"></i
 			></a>
 
 			<!-- Linkedin -->
 			<a
-				class="btn btn-primary btn-floating m-1"
-				style="background-color: #0082ca;"
+				class="btn btn-link btn-floating btn-lg text-dark m-1"
 				href="#!"
 				role="button"
-				><i class="fab fa-linkedin-in"></i
+				data-mdb-ripple-color="dark"
+				><i class="fab fa-linkedin"></i
 			></a>
 			<!-- Github -->
 			<a
-				class="btn btn-primary btn-floating m-1"
-				style="background-color: #333333;"
+				class="btn btn-link btn-floating btn-lg text-dark m-1"
 				href="#!"
 				role="button"
+				data-mdb-ripple-color="dark"
 				><i class="fab fa-github"></i
 			></a>
 			</section>
 			<!-- Section: Social media -->
 		</div>
-
+		<!-- Grid container -->
 
 		<!-- Copyright -->
-		<div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+		<div class="text-center text-dark p-3" style="background-color: rgba(0, 0, 0, 0.2);">
 			© 2020 Copyright:
-			<a class="text-white" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+			<a class="text-dark" href="https://mdbootstrap.com/">MDBootstrap.com</a>
 		</div>
 		<!-- Copyright -->
-		</footer>
+	</footer>
+
 	<script src="js/functions.js"></script>
 	<script src="js/jq.js"></script>
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
